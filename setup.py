470a5aeaf8745a256392b1805901adce98e6310f
ZehanDai <zehan.dai@outlook.com>
2022-07-28 18:21:05 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='biotrinkets',
+        version='0.2',
+        description='small utils for bioinformatic analysis',
+        url='',
+        author='Zehan Dai',
+        author_email='zehan.dai@outlook.com',
+        license='',
+        packages=['fasta_tools','fasta_encoder'],
+        zip_safe=False)
